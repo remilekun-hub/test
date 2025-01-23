6bc7125d6eb62cc32ebc26d047eae37ff1e25899
remilekun-hub <atandaremilekun@gmail.com>
2025-01-23 18:41:22 +0100

    grgrr

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,30 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+// const compat = new FlatCompat({
+// 	baseDirectory: __dirname,
+// });
 
-const eslintConfig = [
-  // ...compat.extends("next/core-web-vitals", "next/typescript"),
+// const eslintConfig = [
+// 	...compat.extends("next/core-web-vitals", "next/typescript"),
+// 	{
+// 		"rules": {
+// 			"no-unused-vars": "off",
+// 		},
+// 	},
+// ];
+
+// export default eslintConfig;
+export default [
+	{
+		rules: {
+			semi: "error",
+			"prefer-const": "error",
+			"no-unused-vars": "off",
+		},
+	},
 ];
-
-export default eslintConfig;
